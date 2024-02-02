c190dcb20f74b409f072c32372c5b9b5235c30c0
lamplight8 <yinjian@sdu.edu.cn>
2024-02-02 15:27:32 +0800

    04_custom_types 20240202

---
+++ c20/03_exceptions.cpp
@@ -0,0 +1,17 @@
+//03_exceptions.cpp
+#include <iostream>
+#include <format>
+
+using namespace std;
+
+int main()
+{
+	try 
+	{
+		cout << format("An integer: {:.}", 5);
+	} 
+	catch (const format_error& caught_exception) 
+	{
+		cout << caught_exception.what(); // "missing precision specifier"
+	}
+}
